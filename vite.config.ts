9e5f64ec63febaf186e46e706509f1675ea07de0
Duc Nguyen <nguyenduc21022k@gmail.com>
2025-08-10 21:55:01 +0700

    add proxy config for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// ! This config only works on local dev environment, on production (Vercel), we use the config in "vercel.json"
+// Please make sure the configuration on two files are in sync
 export default defineConfig({
   plugins: [react()],
   server: {
